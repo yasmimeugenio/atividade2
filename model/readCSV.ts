184aba1554c80a5c0d5cdc9a4bd3015c25c556bb
Yasmim Lopes <Yasmimel30@gmail.com>
2023-10-09 08:23:36 -0300

    alterações

--- model/readCSV.ts
+++ model/readCSV.ts
@@ -7,7 +7,7 @@ export interface Data {
 }
 
 import csv from 'csv-parser';
-import { Data } from './data.interface';
+
 
 const readCSV = async (filePath: string): Promise<Data[]> => {
   return new Promise((resolve, reject) => {
@@ -22,3 +22,22 @@ const readCSV = async (filePath: string): Promise<Data[]> => {
 
 
 export default readCSV;
+
+import { createObjectCsvWriter as createCsvWriter } from 'csv-writer';
+
+const writeCSV = async (filePath: string, data: Data[]): Promise<void> => {
+  const csvWriter = createCsvWriter({
+    path: filePath,
+    header: [
+      { id: 'id', title: 'id' },
+      { id: 'title', title: 'title' },
+      { id: 'value', title: 'value' },
+    ],
+    append: true, // append data to existing file
+  });
+
+  return csvWriter.writeRecords(data);
+};
+
+
+export default writeCSV;
